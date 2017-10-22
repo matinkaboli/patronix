937c0b85113ac41cab22b79ca48241a215fa973a
Ali Movahedi <ali_movahedi@aol.com>
2017-10-22 14:47:57 +0330

    refactor: change getting routers way
    doc: update package.json

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,3 +1,28 @@
-import reqDir from 'require-directory';
+import fs from 'fs';
+import { resolve } from 'path';
 
-export default reqDir(module);
+
+function getRouters(dir) {
+  let result = [];
+  let files = fs.readdirSync(dir);
+
+
+  for (let file of files) {
+    if (file !== 'index.js') {
+      fs.lstatSync(resolve(dir, file)).isFile() ?
+      result.push(resolve(dir, file)) :
+      result.push(...getRouters(resolve(dir, file)));
+    }
+  }
+
+  return result;
+}
+
+let files = getRouters(__dirname);
+let routers = [];
+
+for (let file of files) {
+  routers.push(require(file));
+}
+
+export default routers;
