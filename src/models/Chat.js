33804191194bf3e8fb777961e4e0418c666f20c8
matinkaboli <matinkaboli@aol.com>
2017-10-23 21:54:01 +0330

    Add Chat schema

---
+++ src/models/Chat.js
@@ -0,0 +1,33 @@
+import mongoose, { Schema } from 'mongoose';
+
+const chatSchema = new Schema({
+  chat: [{
+    sender: {
+      enum: [0, 1],
+      type: Number,
+      trim: true,
+      required: [true, "Sender required"]
+    },
+    message: {
+      type: String,
+      trim: true,
+      required: [true, "Message required"]
+    },
+    time: {
+      type: Number,
+      trim: true,
+      default: Date.now()
+    }
+  }],
+  site: {
+
+  },
+  operator: {
+
+  },
+  client: {
+
+  }
+}, { versionKey: false });
+
+export default mongoose.model('Chat', chatSchema);
