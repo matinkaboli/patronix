3c5b96b5f8cc2fd05bb133173c04db52ff6ff3a0
matinkaboli <matinkaboli@aol.com>
2017-10-23 21:53:23 +0330

    Add Operator schema

---
+++ src/models/Operator.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from 'mongoose';
+
+const operatorSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    required: [true, "Email required"]
+  },
+  password: {
+    type: String,
+    trim: true
+  }
+}, { versionKey: false });
+
+export default mongoose.model('Operator', operatorSchema);
