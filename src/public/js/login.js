6c4f068974df74769f3f65575e2e8d1e4c516566
matinkaboli <matinkaboli@aol.com>
2017-10-23 21:49:57 +0330

    Add public file and login.js

---
+++ src/public/js/login.js
@@ -0,0 +1,34 @@
+const loginForm = document.forms["login-form"];
+
+function validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+function validateUsername(username) {
+  const re = /^[a-zA-Z0-9]+([_ .]?[a-zA-Z0-9])*$/;
+  return re.test(username);
+}
+function validatePassword(password) {
+  const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+  return re.test(password);
+}
+function checkForm() {
+  const email = loginForm.email;
+  const password = loginForm.password;
+  alert("Hello")
+  if (!validatePassword(password.value)) {
+    alert(`Your password should have at lease one
+  number, upper case letter and lower case letter and symbol
+  and at least 8 number.`);
+    return false;
+  }
+  if (!validateEmail(email.value)) {
+    alert("Write a correct email.");
+    return false;
+  }
+  if (password.value.length < 8) {
+    alert("Password is too short.");
+    return false;
+  }
+  return true;
+}
