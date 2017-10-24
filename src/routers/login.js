dfc8d18b1e053e6ece424d186e29f490393a7ec3
matinkaboli <matinkaboli@aol.com>
2017-10-24 21:44:20 +0330

    feat: use express-rate-limit for login page

--- src/routers/login.js
+++ src/routers/login.js
@@ -1,12 +1,22 @@
 import { Router } from 'express';
+import RateLimit from 'express-rate-limit';
 
 const router = new Router();
+
+const loginLimiter = new RateLimit({
+  windowMs: 1000 * 60 * 60 * 3,
+  max: 50,
+  delayMs: 300,
+  handler(req, res) {
+    res.render('too_many_req.njk');
+  }
+});
 
 router.get('/login', (req, res) => {
   res.render('login.njk');
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', loginLimiter, (req, res) => {
   res.json(req.body);
 });
 
