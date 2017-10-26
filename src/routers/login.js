fa2dba920232aff46d14bf1b703c23d1c2d919df
matinkaboli <matinkaboli@aol.com>
2017-10-26 13:35:26 +0330

    fix: check login and make code if user does not have

--- src/routers/login.js
+++ src/routers/login.js
@@ -1,5 +1,8 @@
 import { Router } from 'express';
 import RateLimit from 'express-rate-limit';
+import { User, Code } from '../models';
+import { encrypt } from '../utils/encrypt';
+import { unique } from 'stringing';
 
 const router = new Router();
 
@@ -17,7 +20,38 @@ router.get('/login', (req, res) => {
 });
 
 router.post('/login', loginLimiter, (req, res) => {
-  res.json(req.body);
+  req.body.email = req.body.email.toLowerCase();
+
+  User.find({
+    email: req.body.email,
+    password: encrypt(req.body.password, req.body.email)
+  }).then(userDoc => {
+    if (JSON.stringify(userDoc) === '[]') {
+      res.send('User not found');
+    } else {
+      if (userDoc[0].status === 0) {
+        Code.find({ user: userDoc[0]._id }).then(codeDoc => {
+          if (JSON.stringify(codeDoc) === '[]') {
+            const newCode = new Code({
+              code: unique(25),
+              user: userDoc[0]._id
+            });
+            newCode.save().then(() => {
+              res.send('You have to verify your fucking email');
+            }).catch(() => {
+              res.send('Error');
+            });
+          } else {
+            res.send('You have to verify your fucking email');
+          }
+        }).catch(() => {
+          res.send('Error');
+        });
+      } else {
+        res.send('Your account was verified before :)');
+      }
+    }
+  });
 });
 
 export default router;
