575d613943a0b19293477606f15579222069ae44
matinkaboli <matinkaboli@aol.com>
2017-11-05 15:54:08 +0330

    feat: add logout page

---
+++ src/routers/logout.js
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { auth } from '../utils/UserManager';
+
+const router = new Router();
+
+router.get('/logout', auth, (req, res) => {
+  req.session.user;
+  req.flash('success', 'شما با موفقیت از حسابتان خارج شدید.');
+  res.redirect('/');
+});
+
+export default router;
