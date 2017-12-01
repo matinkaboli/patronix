f4de0aa6a627acda1fb439fadcfeca56b8c7e87c
Ali Movahedi <ali_movahedi@aol.com>
2017-12-01 22:11:15 +0330

    fix: fix add perm

--- src/perms/u/site/operators/add.js
+++ src/perms/u/site/operators/add.js
@@ -1,9 +1,26 @@
-const { Site } = rootRequire('./models');
+const { Site, User } = rootRequire('./models');
+
+function isUnique(array, unique) {
+  for (let item of array) {
+    if (unique.toString() === item.toString()) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 export default (req, res, next) => {
-  Site.findOne({ _id: req.params.id }).then(site => {
-    if (site.operators.length < 2) {
-      next();
+  Site.findOne({ _id: req.params.id })
+  .then(site => {
+    if (site.operators.length < 6) {
+      User.findOne({ email: req.body.email }).then(user => {
+        if (isUnique(site.operators, user._id)) {
+          next();
+        } else {
+          res.reply.error();
+        }
+      });
     } else {
       res.reply.error();
     }
