66c7100cb8b3a693cae919923e1d585e46d904d3
Ali Movahedi <ali_movahedi@aol.com>
2017-12-01 19:33:40 +0330

    feat: add router operators.js
    feat: add template operators.njk

---
+++ src/routers/u/site/operators/operators.js
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+
+const perms = rootRequire('./perms');
+const { Site } = rootRequire('./models');
+
+const router = new Router();
+
+router.get(
+  '/u/site/:id/operators/',
+  perms.logged,
+  perms.u.site.isOwner,
+  (req, res) => {
+    Site
+    .findOne({ _id: req.params.id })
+    .populate('operators')
+    .exec()
+    .then(site => {
+      res.render(
+        'u/site/operators/operators.njk',
+        { operators: site.operators }
+      );
+    });
+  }
+);
+
+export default router;
