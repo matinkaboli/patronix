ee61475671a3534e4b15fffa50fc92cd2415e693
Ali Movahedi <ali_movahedi@aol.com>
2017-12-02 12:42:53 +0330

    feat: add operators/remove

---
+++ src/routers/u/site/operators/remove.js
@@ -0,0 +1,55 @@
+import { Router } from 'express';
+
+const perms = rootRequire('./perms');
+const { Site } = rootRequire('./models');
+
+const router = new Router();
+
+router.get(
+  '/u/site/:id/operators/remove',
+  perms.logged,
+  perms.u.site.isOwner,
+  (req, res) => {
+    Site
+    .findOne({ _id: req.params.id })
+    .populate('operators')
+    .exec()
+    .then(site => {
+      res.render(
+        'u/site/operators/remove.njk',
+        {
+          id: site._id,
+          operators: site.operators
+        }
+      );
+    });
+  }
+);
+
+router.post(
+  '/u/site/:id/operators/remove',
+  perms.logged,
+  perms.u.site.isOwner,
+  (req, res) => {
+    Site.findOne({ _id: req.params.id }).then(site => {
+      let index = 0;
+      for (let [i, operator] of site.operators.entries()) {
+        if (req.body.id === operator.toString()) {
+          index = i;
+          break;
+        }
+      }
+
+      site.operators = [
+        ...site.operators.slice(0, index),
+        ...site.operators.slice(index + 1, site.operators.length)
+      ];
+
+      site.save().then(() => {
+        res.reply.ok();
+      });
+    });
+  }
+);
+
+export default router;
