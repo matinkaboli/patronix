3b58c0e7b8f0de8364c928b77cc452fbca5f8de5
matinkaboli <matinkaboli@aol.com>
2017-12-02 20:36:44 +0330

    change passwordsetting to password in setting

--- src/routers/u/setting/setting.js
+++ src/routers/u/setting/setting.js
@@ -3,7 +3,6 @@ import { Router } from 'express';
 const router = new Router();
 const perms = rootRequire('./perms');
 const { User } = rootRequire('./models');
-const { encrypt } = rootRequire('./utils');
 
 router.get('/u/setting', perms.logged, (req, res) => {
   if (req.session.user) {
@@ -86,39 +85,6 @@ router.post('/u/setting', perms.logged, (req, res) => {
     }).catch(() => {
       req.flash('error', 'خطا! بعدا امتحان کنید.');
       res.redirect('/u');
-    });
-  } else {
-    req.flash('error', 'خطا! بعدا امتحان کنید.');
-    res.redirect('/setting');
-  }
-});
-
-router.post('/u/setting/settingpassword', perms.logged, (req, res) => {
-  if (req.body.oldpass && req.body.newpass) {
-    User.findOne({ _id: req.session.user }).then(user => {
-      if (user) {
-        if (encrypt(req.body.oldpass, user.email) === user.password) {
-          user.password = encrypt(req.body.newpass, user.email);
-
-          user.save().then(() => {
-            req.flash('success', 'رمز شما با موفقیت تغییر یافت.');
-            res.redirect('/u');
-          }).catch(() => {
-            req.flash('error', 'خطا! بعدا امتحان کنید.');
-            res.redirect('/setting');
-          });
-        }
-        else {
-          req.flash('error', 'رمز وارد شده اشتباه میباشد.');
-          res.redirect('/setting');
-        }
-      } else {
-        req.flash('error', 'خطا! بعدا امتحان کنید.');
-        res.redirect('/u');
-      }
-    }).catch(() => {
-      req.flash('error', 'خطا! بعدا امتحان کنید.');
-      res.redirect('/setting');
     });
   } else {
     req.flash('error', 'خطا! بعدا امتحان کنید.');
