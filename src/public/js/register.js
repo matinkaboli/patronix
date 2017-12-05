b76d7f125e52ad8d01ed96b0e6c88c3625c3747c
matinkaboli <matinkaboli@aol.com>
2017-12-05 22:28:22 +0330

    refactor: move validator to lib/ and utils/ and remove validatePassword from it

--- src/public/js/register.js
+++
@@ -1,8 +0,0 @@
-const registerForm = document.forms['signup-form'];
-
-function checkForm() {
-  const email = registerForm.email;
-  const password = registerForm.password;
-
-  return true;
-}
--- src/public/js/validator.js
+++
@@ -1,8 +0,0 @@
-function validateEmail(email) {
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email);
-}
-function validatePassword(password) {
-  const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
-  return re.test(password);
-}
