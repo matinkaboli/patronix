8ff7a3459d7cc29ed08c9b640e3539dd2f05c935
Ali Movahedi <ali_movahedi@aol.com>
2017-12-06 22:38:51 +0330

    fix: add error handling to signup and resend
    feat: add README for better understanding

--- src/routers/resend.js
+++ src/routers/resend.js
@@ -2,10 +2,11 @@ import { Router } from 'express';
 import RateLimit from 'express-rate-limit';
 import { unique } from 'stringing';
 // import send from '../utils/mail';
+
 const { login } = rootRequire('./perms');
+const { User, Link } = rootRequire('./models');
 
 const router = new Router();
-const { User, Link } = rootRequire('./models');
 
 const limiter = new RateLimit({
   windowMs: 1000 * 60 * 60 * 3,
@@ -29,12 +30,14 @@ router.post('/resend', login, limiter, (req, res) => {
     User.findOne({ email: req.body.email }).then(user => {
       if (user) {
         if (user.status === 0) {
-          Link.findOne({ user: user._id }).then(code => {
-            if (code) {
+          Link.findOne({ user: user._id }).then(link => {
+            if (link) {
               // send(req.body.email, code.link, 'resend', user.fname);
 
-              res.json({ type: 1, text: 1 });
-            } else {
+              res.json({ type: 2, text: 1 });
+            }
+
+            else {
               const newLink = new Link({
                 user: user._id,
                 link: unique(25)
@@ -44,29 +47,26 @@ router.post('/resend', login, limiter, (req, res) => {
                 // send(req.body.email, newLink.link, 'resend', user.fname);
 
                 // sent
-                res.json({ type: 1, text: 1 });
-              }).catch(() => {
-                // error
-                res.json({ type: 0, text: 1 });
+                res.json({ type: 2, text: 1 });
               });
             }
-          }).catch(() => {
-            // error
-            res.json({ type: 0, text: 1 });
           });
-        } else {
+        }
+
+        else {
           // verified before
           res.json({ type: 2, text: 0 });
         }
-      } else {
+      }
+
+      else {
         // no such user
         res.json({ type: 0, text: 0 });
       }
-    }).catch(() => {
-      // error
-      res.json({ type: 0, text: 1 });
     });
-  } else {
+  }
+
+  else {
     // error
     res.json({ type: 0, text: 1 });
   }
