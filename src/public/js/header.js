a7f5c3e128777d63a52239c766ebf9f725908037
Matin Kaboli <matinkaboli@aol.com>
2017-12-09 00:30:02 +0330

    fix: bug for header icon

--- src/public/js/header.js
+++ src/public/js/header.js
@@ -2,6 +2,7 @@ const d = document.getElementById('open-close-bar');
 const dO = document.getElementById('open-bar');
 const dC = document.getElementById('close-bar');
 let time = 0;
+
 d.addEventListener('click', () => {
   if (time === 0) {
     dO.style.display = 'none';
@@ -13,15 +14,16 @@ d.addEventListener('click', () => {
     time = 0;
   }
 });
-window.addEventListener('resize', () => {
-  const list = document.getElementById('open-close-bar');
-  const listLinks = document.getElementById('list-links');
 
-  if (window.outerWidth > 580) {
-    list.style.display = 'none';
-    listLinks.style.display = 'none';
-  } else {
-    listLinks.style.display = 'block';
-    list.style.display = 'block';
-  }
-});
+// window.addEventListener('resize', () => {
+//   const list = document.getElementById('open-close-bar');
+//   const listLinks = document.getElementById('list-links');
+//
+//   if (window.outerWidth > 580) {
+//     list.style.display = 'none';
+//     listLinks.style.display = 'none';
+//   } else {
+//     listLinks.style.display = 'block';
+//     list.style.display = 'block';
+//   }
+// });
