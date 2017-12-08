96aa5bc06509eb0519e73dcab8d4367ff797f5c9
Ali Movahedi <ali_movahedi@aol.com>
2017-12-08 11:36:23 +0330

    refactor: seperate client and server building
    refactor: remove deprecated package

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,124 @@
 
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
-var rm = require('gulp-rimraf');
 var less = require('gulp-less');
 var autoprefixer = require('gulp-autoprefixer');
 var minifier = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
 var babel = require('gulp-babel');
-var config = require('./src/config.json');
 var htmlmin = require('gulp-htmlmin');
 var replace = require('gulp-replace');
+var del = require('del');
 
 var config = require('./src/config.json');
 
-gulp.task('clean', function() {
-  return gulp.src('build', { read: false })
-        .pipe(rm());
+/**
+ * functions
+ */
+
+function taskBabel() {
+  return gulp.src(['src/**/*.js', '!src/public/js/lib/**/*', '!src/public/js/**/*.js'])
+  .pipe(babel({
+    presets: [
+      'env'
+    ],
+    plugins: [
+      'add-module-exports',
+      'transform-runtime'
+    ]
+  }))
+  .pipe(replace(/@@LINK@@/g, config.url))
+  .pipe(gulp.dest('build'));
+}
+
+function taskJsProd() {
+  return gulp.src(['src/public/js/**/*.js', '!src/public/js/lib/**/*'])
+  .pipe(babel({
+    presets: [
+      'env'
+    ]
+  }))
+  .pipe(uglify())
+  .pipe(gulp.dest('build/public/js'));
+}
+
+function taskJsDev() {
+  return gulp.src(['src/public/js/**/*.js', '!src/public/js/lib/**/*'])
+  .pipe(babel({
+    presets: [
+      'env'
+    ]
+  }))
+  .pipe(gulp.dest('build/public/js'));
+}
+
+function taskLess() {
+  return gulp.src('src/public/css/*.less')
+  .pipe(less())
+  .pipe(autoprefixer())
+  .pipe(minifier())
+  .pipe(gulp.dest('build/public/css'));
+}
+
+function taskNunjucksProd() {
+  gulp.src('src/views/**/*.njk')
+      .pipe(replace(/@@PROJECTNAME@@/g, config.title))
+      .pipe(htmlmin({ collapseWhitespace: true }))
+      .pipe(gulp.dest('build/views'));
+}
+
+function taskNunjucksDev() {
+  gulp.src('src/views/**/*.njk')
+      .pipe(replace(/@@PROJECTNAME@@/g, config.title))
+      .pipe(gulp.dest('build/views'));
+}
+
+/**
+ * tasks
+ */
+
+gulp.task('server:clean', function() {
+  return del([
+    'build/**',
+    '!build',
+    '!build/public',
+    '!build/public/**'
+  ]);
 });
 
-gulp.task('copy', function() {
+gulp.task('client:clean', function() {
+  return del([
+    'build/public/**',
+    '!build/public',
+    '!build/public/font',
+    '!build/public/font/**',
+    '!build/public/image',
+    '!build/public/image/**'
+  ]);
+});
+
+gulp.task('full:clean', function() {
+  return del('build');
+});
+
+gulp.task('server:copy', function() {
+  return [
+    gulp.src(['src/config.json'])
+    .pipe(gulp.dest('build'))
+  ];
+});
+
+gulp.task('client:copy', function() {
+  return [
+    gulp.src(['src/public/**/*', '!src/public/js/**/*', '!src/public/css/**/*'])
+    .pipe(gulp.dest('build/public/')),
+
+    gulp.src(['src/public/js/lib/**'])
+    .pipe(gulp.dest('build/public/js/lib'))
+  ];
+});
+
+gulp.task('full:copy', function() {
   return [
     gulp.src(['src/config.json'])
     .pipe(gulp.dest('build')),
@@ -32,41 +132,13 @@ gulp.task('copy', function() {
   ];
 });
 
-gulp.task('babel', ['clean'], function() {
-  return gulp.src(['src/**/*.js', '!src/public/js/lib/**/*', '!src/public/js/**/*.js'])
-        .pipe(babel({
-          presets: [
-            'env'
-          ],
-          plugins: [
-            'add-module-exports',
-            'transform-runtime'
-          ]
-        }))
-        .pipe(replace(/@@LINK@@/g, config.url))
-        .pipe(gulp.dest('build'));
-});
+gulp.task('server:babel', ['server:clean'], taskBabel);
+gulp.task('full:babel', ['full:clean'], taskBabel);
 
-gulp.task('js:prod', ['clean'], function() {
-  return gulp.src(['src/public/js/**/*.js', '!src/public/js/lib/**/*'])
-        .pipe(babel({
-          presets: [
-            'env'
-          ]
-        }))
-        .pipe(uglify())
-        .pipe(gulp.dest('build/public/js'))
-});
-
-gulp.task('js:dev', ['clean'], function() {
-  return gulp.src(['src/public/js/**/*.js', '!src/public/js/lib/**/*'])
-        .pipe(babel({
-          presets: [
-            'env'
-          ]
-        }))
-        .pipe(gulp.dest('build/public/js'))
-});
+gulp.task('client:js:prod', ['client:clean'], taskJsProd);
+gulp.task('full:js:prod', ['full:clean'], taskJsProd);
+gulp.task('client:js:dev', ['client:clean'], taskJsDev);
+gulp.task('js:dev', ['full:clean'], taskJsDev);
 
 gulp.task('lint', function() {
   return gulp.src('src/**/*.js')
@@ -75,35 +147,44 @@ gulp.task('lint', function() {
         .pipe(eslint.failAfterError());
 });
 
-gulp.task('less', ['clean'], function() {
-  return gulp.src('src/public/css/*.less')
-        .pipe(less())
-        .pipe(autoprefixer())
-        .pipe(minifier())
-        .pipe(gulp.dest('build/public/css'));
+gulp.task('client:less', ['client:clean'], taskLess);
+gulp.task('full:less', ['full:clean'], taskLess);
+
+gulp.task('client:nunjucks:prod', ['client:clean'], taskNunjucksProd);
+gulp.task('server:nunjucks:prod', ['server:clean'], taskNunjucksProd);
+gulp.task('full:nunjucks:prod', ['full:clean'], taskNunjucksProd);
+gulp.task('client:nunjucks:dev', ['client:clean'], taskNunjucksDev);
+gulp.task('server:nunjucks:dev', ['server:clean'], taskNunjucksDev);
+gulp.task('full:nunjucks:dev', ['full:clean'], taskNunjucksDev);
+
+
+gulp.task('client:prod', ['client:clean', 'client:less', 'client:nunjucks:prod', 'client:js:prod'], function() {
+  return gulp.start('client:copy');
+});
+gulp.task('client:dev', ['client:clean', 'client:less', 'client:nunjucks:dev', 'client:js:dev'], function() {
+  return gulp.start('client:copy');
 });
 
-gulp.task('nunjucks:prod', ['clean'], function() {
-  gulp.src('src/views/**/*.njk')
-      .pipe(replace(/@@PROJECTNAME@@/g, config.title))
-      .pipe(htmlmin({ collapseWhitespace: true }))
-      .pipe(gulp.dest('build/views'));
+gulp.task('server:prod', ['server:clean', 'server:babel', 'server:nunjucks:prod'], function() {
+  return gulp.start('server:copy');
+});
+gulp.task('server:dev', ['server:clean', 'server:babel', 'server:nunjucks:dev'], function() {
+  return gulp.start('server:copy');
 });
 
-gulp.task('nunjucks:dev', ['clean'], function() {
-  gulp.src('src/views/**/*.njk')
-      .pipe(replace(/@@PROJECTNAME@@/g, config.title))
-      .pipe(gulp.dest('build/views'));
+gulp.task('full:prod', ['full:clean', 'full:babel', 'full:less', 'full:nunjucks:prod', 'full:js:prod'], function() {
+  return gulp.start('full:copy');
 });
+gulp.task('full:dev', ['full:clean', 'full:babel', 'full:less', 'full:nunjucks:dev', 'full:js:dev'], function() {
+  return gulp.start('full:copy');
+})
 
-gulp.task('prod', ['clean', 'lint', 'babel', 'less', 'nunjucks:prod', 'js:prod'], function() {
-  return gulp.start('copy');
+gulp.task('client:watch', function() {
+  gulp.watch('src/**/*', ['client:dev']);
 });
-
-gulp.task('dev', ['clean', 'babel', 'less', 'nunjucks:dev', 'js:dev'], function() {
-  return gulp.start('copy');
+gulp.task('server:watch', function() {
+  gulp.watch('src/**/*', ['server:dev']);
 });
-
-gulp.task('watch', function() {
-  gulp.watch('src/**/*', ['dev']);
+gulp.task('full:watch', function() {
+  gulp.watch('src/**/*', ['full:dev']);
 });
