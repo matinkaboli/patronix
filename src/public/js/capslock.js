a45672afaa099b0bee21ad81222ab403e8546dfb
Matin Kaboli <matinkaboli@aol.com>
2017-12-13 23:29:23 +0330

    fix: fix capslock warning bug fix: add samim font to inputs and buttons feat: animation to button

--- src/public/js/capslock.js
+++ src/public/js/capslock.js
@@ -1,10 +1,14 @@
-document.getElementById('password').addEventListener('keypress', e => {
-  const kc = e.keyCode ? e.keyCode : e.which;
-  const sk = e.shiftKey ? e.shiftKey : ((kc === 16) ? true : false);
-  const msg = document.getElementById('capslock');
-  if (((kc >= 65 && kc <= 90) && !sk) || ((kc >= 97 && kc <= 122) && sk)) {
-    msg.style.display = 'block';
-  } else {
-    msg.style.display = 'none';
-  }
-});
+const pass = document.getElementsByClassName('password');
+
+for (let i = 0; i < pass.length; i++) {
+  pass[i].addEventListener('keypress', e => {
+    const kc = e.keyCode ? e.keyCode : e.which;
+    const sk = e.shiftKey ? e.shiftKey : ((kc === 16) ? true : false);
+    const msg = document.getElementsByClassName('capslock');
+    if (((kc >= 65 && kc <= 90) && !sk) || ((kc >= 97 && kc <= 122) && sk)) {
+      msg[i].style.display = 'block';
+    } else {
+      msg[i].style.display = 'none';
+    }
+  });
+}
