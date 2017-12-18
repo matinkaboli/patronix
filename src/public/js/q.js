33142680917ce9bf7f1fc84b75607773fbf1e0ba
Ali Movahedi <ali_movahedi@aol.com>
2017-12-18 22:28:16 +0330

    refactor: change mechanism Q

--- src/public/js/q.js
+++ src/public/js/q.js
@@ -1,80 +1,93 @@
-class Q {
-  constructor(el) {
-    this.el = el;
-  }
+function Q(el) {
+  class QGen {
+    constructor(el) {
+      this.el = document.querySelector(el);
+    }
 
-  hide() {
-    this.el.style.display = 'none';
-  }
+    hide() {
+      this.el.style.display = 'none';
+    }
 
-  show() {
-    this.el.style.display = '';
-  }
+    show() {
+      this.el.style.display = '';
+    }
 
-  append(child) {
-    this.el.appendChild(child);
-  }
+    append(child) {
+      this.el.appendChild(child);
+    }
 
-  appendText(txt) {
-    this.el.innerHTML += txt;
-  }
+    appendText(txt) {
+      this.el.innerHTML += txt;
+    }
 
-  empty() {
-    this.el.innerHTML = '';
-  }
+    empty() {
+      this.el.innerHTML = '';
+    }
 
-  attr(key, val) {
-    if (val) {
-      this.el.setAttribute(key, val);
-    } else {
-      return this.el.getAttribute(key);
+    attr(key, val) {
+      if (val) {
+        this.el.setAttribute(key, val);
+      } else {
+        return this.el.getAttribute(key);
+      }
+    }
+
+    css(key, val) {
+      if (val) {
+        this.el.style[key] = val;
+      } else {
+        return this.el.style[key];
+      }
+    }
+
+    get text() {
+      return this.el.textContent;
+    }
+
+    get position() {
+      return {
+        l: this.el.offsetLeft,
+        t: this.el.offsetTop
+      };
+    }
+
+    prepend(child) {
+      this.el.insertBefore(child, this.el.firstChild);
+    }
+
+    removeClass(cls) {
+      this.el.classList.remove(cls);
+    }
+
+    addClass(cls) {
+      this.el.classList.add(cls);
+    }
+
+    html(str = '') {
+      this.el.innerHTML = str;
+    }
+
+    toggleClass(cls) {
+      this.el.classList.toggle(cls);
+    }
+
+    off(e, fn) {
+      this.el.removeEventListener(e, fn);
+    }
+
+    on(e, fn) {
+      this.el.addEventListener(e, fn);
     }
   }
 
-  css(key, val) {
-    if (val) {
-      this.el.style[key] = val;
-    } else {
-      return this.el.style[key];
+  let q = new QGen(el);
+
+  return new Proxy(
+    q,
+    {
+      get(target, name) {
+        return name in target ? target[name] : target.el[name];
+      }
     }
-  }
-
-  get text() {
-    return this.el.textContent;
-  }
-
-  get position() {
-    return {
-      l: this.el.offsetLeft,
-      t: this.el.offsetTop
-    };
-  }
-
-  prepend(child) {
-    this.el.insertBefore(child, this.el.firstChild);
-  }
-
-  removeClass(cls) {
-    this.el.classList.remove(cls);
-  }
-
-  addClass(cls) {
-    this.el.classList.add(cls);
-  }
-
-  html(str = '') {
-    this.el.innerHTML = str;
-  }
-
-  toggleClass(cls) {
-    this.el.classList.toggle(cls);
-  }
-
-  off(e, fn) {
-    this.el.removeEventListener(e, fn);
-  }
-
-  on(e, fn) {
-    this.el.addEventListener(e, fn);
-  }
+  );
 }
