dd34a33fb52d3693c83878f566c9f1d8dbbf4871
Matin Kaboli <matinkaboli@aol.com>
2017-12-21 13:20:55 +0330

    feat: add password validator to signup

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -18,11 +18,13 @@ f.addEventListener('submit', e => {
   const taken = $('#taken');
   const err = $('#err');
   const emailErr = $('#email-err');
+  const passErr = $('#pass-err');
 
   err.hide();
   emailErr.hide();
   captchaErr.hide();
   taken.hide();
+  passErr.hide();
 
   if (
     f.email.value &&
@@ -31,40 +33,47 @@ f.addEventListener('submit', e => {
     if (!validateEmail(f.email.value)) {
       emailErr.show();
     } else {
-      fetch('/signup', {
-        method: 'POST',
-        credentials: 'include',
-        headers: new Headers({
-          'Content-Type': 'application/json'
-        }),
-        body: JSON.stringify({
-          password: f.password.value,
-          email: f.email.value,
-          fname: f.fname.value,
-          lname: f.lname.value,
-          captcha: f.captcha.value
-        })
-      }).then(checkStatus).then(res => res.json()).then(data => {
-        if (data.type === 0) {
-          if (data.text === 0) {
-            // unverified
-            captchaErr.show();
-          } else if (data.text === 1) {
-            // account has expired
-          } else if (data.text === 2) {
-            // wrong pass or no such user
-            taken.show();
-          } else if (data.text === 3) {
-            // error occured
-            err.show();
+
+      if (f.password.value.length > 7) {
+        fetch('/signup', {
+          method: 'POST',
+          credentials: 'include',
+          headers: new Headers({
+            'Content-Type': 'application/json'
+          }),
+          body: JSON.stringify({
+            password: f.password.value,
+            email: f.email.value,
+            fname: f.fname.value,
+            lname: f.lname.value,
+            captcha: f.captcha.value
+          })
+        }).then(checkStatus).then(res => res.json()).then(result => {
+          const data = result.report;
+          console.log(data);
+          if (data.type === 0) {
+            if (data.text === 0) {
+              // unverified
+              captchaErr.show();
+            } else if (data.text === 1) {
+              // account has expired
+            } else if (data.text === 2) {
+              // wrong pass or no such user
+              taken.show();
+            } else if (data.text === 3) {
+              // error occured
+              err.show();
+            }
+          } else if (data.type === 2) {
+            window.location.href = '/u';
           }
-        } else if (data.type === 2) {
-          window.location.href = '/u';
-        }
-      }).catch(() => {
-        // error occured
-        err.show();
-      });
+        }).catch(() => {
+          // error occured
+          err.show();
+        });
+      } else {
+        passErr.show();
+      }
     }
   }
 });
