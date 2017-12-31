25510f7cd91e8d833ebfe3757f312e52ad654cf5
Matin Kaboli <matinkaboli@aol.com>
2017-12-31 14:30:26 +0330

    fix: remove capslock from login.js and use function instead

--- src/public/js/resend.js
+++ src/public/js/resend.js
@@ -1,50 +1,39 @@
-// This file need to be changed
-function checkForm() { //eslint-disable-line
-  const f = document.forms['resend-form'];
+const f = document.forms['resend-form'];
 
-  if (f.email.value) {
-    if (!validateEmail(f.email.value)) {
-      const p = document.createElement('p');
-      p.innerHTML = M[1][0];
-      divW.appendChild(p);
-      divW.style.display = 'block';
-    } else {
-      fetch('/resend', {
-        method: 'POST',
-        credentials: 'include',
-        headers: new Headers({
-          'Content-Type': 'application/json'
-        }),
-        body: JSON.stringify({
-          email: f.email.value
-        })
-      }).then(checkStatus).then(res => res.json()).then(data => {
-        if (data.type === 'e') {
-          const p = document.createElement('p');
-          if (data.code === 0) {
-            p.innerHTML = M[0][3];
-          } else if (data.code === 1) {
-            p.innerHTML = M[0][0];
-          }
-          divE.appendChild(p);
-          divE.style.display = 'block';
-        } else if (data.type === 's') {
-          const p = document.createElement('p');
-          p.innerHTML = M[2][1];
-          divS.appendChild(p);
-          divS.style.display = 'block';
-        } else if (data.type === 'w') {
-          if (data.code === 0) {
-            window.location.href = '/login';
-          }
+f.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const err = $('#err');
+  const emailErr = $('#emailErr');
+  const success = $('#success');
+
+  if (validateEmail(f.email.value)) {
+    fetch('/resend', {
+      method: 'POST',
+      credentials: 'include',
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      }),
+      body: JSON.stringify({
+        email: f.email.value
+      })
+    }).then(checkStatus).then(res => res.json()).then(data => {
+      if (data.type === 0) {
+        if (data.code === 0) {
+          err.show();
+        } else if (data.code === 1) {
+          err.show();
         }
-      }).catch(() => {
-        const p = document.createElement('p');
-        p.innerHTML = M[0][0];
-        divE.appendChild(p);
-        divE.style.display = 'block';
-      });
-    }
+      } else if (data.type === 2) {
+        success.show();
+        window.location.href = '/login';
+      } else if (data.type === 1) {
+        success.show();
+      }
+    }).catch(() => {
+      err.show();
+    });
+  } else {
+    emailErr.show();
   }
-  return false;
-}
+});
