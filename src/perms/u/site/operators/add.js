7ac41ebddb1f1751fe97407007fad68067f0d9a0
Ali Movahedi <ali_movahedi@aol.com>
2018-01-21 01:56:43 +0330

    fix: fix crash because sending empty email

--- src/perms/u/site/operators/add.js
+++ src/perms/u/site/operators/add.js
@@ -4,18 +4,24 @@ export default (req, res, next) => {
   let site = req.middle.site;
   if (site.operators.length < 3) {
     User.findOne({ email: req.body.email }, { operators: 1 }).then(user => {
-      let isUnique = true;
-      for (let operator of site.operators) {
-        if (user._id.toString() === operator.toString()) {
-          isUnique = false;
-          break;
+      if (user) {
+        let isUnique = true;
+        for (let operator of site.operators) {
+          if (user._id.toString() === operator.toString()) {
+            isUnique = false;
+            break;
+          }
+        }
+
+        if (isUnique) {
+          next();
+        } else {
+          res.json({ type: 0, text: 0 });
         }
       }
 
-      if (isUnique) {
-        next();
-      } else {
-        res.reply.error();
+      else {
+        res.json({ type: 0, text: 0 });
       }
     });
   } else {
