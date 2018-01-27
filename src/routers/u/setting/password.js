3882322bf990cb913f816eadbd042ae3964faad7
Ali Movahedi <ali_movahedi@aol.com>
2018-01-27 20:11:47 +0330

    fix: fix changing password and using new alg

--- src/routers/u/setting/password.js
+++ src/routers/u/setting/password.js
@@ -3,13 +3,14 @@ import { Router } from 'express';
 const router = new Router();
 const { logged } = rootRequire('./perms');
 const { User } = rootRequire('./models');
-const { encrypt } = rootRequire('./utils');
+const { decrypt, encrypt } = rootRequire('./utils');
+const { dbkey } = rootRequire('./config');
 
 router.post('/u/setting/password', logged, (req, res) => {
   if (req.body.oldpass && req.body.newpass) {
     User.findOne({ _id: req.user.user._id }).then(user => {
       if (user) {
-        if (encrypt(req.body.oldpass, user.email) === user.password) {
+        if (req.body.oldpass === decrypt(user.password, user.email + dbkey)) {
           user.password = encrypt(req.body.newpass, user.email);
 
           user.save().then(() => {
