36b4c7da21f698c1e2737e86b98e141202ebb3fb
Matin Kaboli <matinkaboli@aol.com>
2018-01-29 21:29:16 +0330

    feat: show messages in client

--- src/public/js/test-client.js
+++ src/public/js/test-client.js
@@ -1,4 +1,6 @@
 (() => {
+  var msgs = document.getElementById('patronix-chatlist');
+
   let socketUrl = `http://localhost:8010/client?token=${token}`;
 
   let socket = io(socketUrl);
@@ -10,9 +12,46 @@
   let text = document.getElementById('chat');
   document.getElementById('send').addEventListener('click', () => {
     socket.emit('message', text.value);
+
+    const d = document.createElement('div');
+    d.classList.add('message-box');
+
+    const date = new Date();
+    const time = `${date.getHours()}:${date.getMinutes()}`;
+    const p = document.createElement('p');
+    p.classList.add('message-time');
+    p.innerHTML = time;
+
+    const msg = document.createElement('p');
+    msg.classList.add('chat-message');
+    msg.innerHTML = text.value;
+
+    d.appendChild(msg);
+    d.appendChild(p);
+
+    msgs.appendChild(d);
   });
 
   socket.on('message', message => {
+    const d = document.createElement('div');
+    d.classList.add('message-box');
+    d.classList.add('op');
+
+    const date = new Date();
+    const time = `${date.getHours()}:${date.getMinutes()}`;
+    const p = document.createElement('p');
+    p.classList.add('message-time');
+    p.innerHTML = time;
+
+    const msg = document.createElement('p');
+    msg.classList.add('chat-message');
+    msg.innerHTML = message;
+
+    d.appendChild(msg);
+    d.appendChild(p);
+
+    msgs.appendChild(d);
+
     console.log(message);
   });
 })();
