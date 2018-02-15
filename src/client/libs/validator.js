05eb51507bdd8b706c1473e8141176900a7f6dfb
Matin Kaboli <matinkaboli@aol.com>
2018-02-15 13:02:00 +0330

    feat: password and email validate in signup page

--- src/client/libs/validator.js
+++ src/client/libs/validator.js
@@ -1,5 +1,13 @@
 function validateEmail(email){var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;return re.test(email);}
 
+function validatePassword(password) {
+  if (password.length < 8) {
+    return false;
+  }
+  return true;
+}
+
 export {
-  validateEmail as e
+  validateEmail as e,
+  validatePassword as p
 };
