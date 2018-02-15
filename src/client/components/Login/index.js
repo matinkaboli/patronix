e9c20033453ed1d1a0fdc9e3c8940e6d9552e95e
Matin Kaboli <matinkaboli@aol.com>
2018-02-15 12:55:12 +0330

    feat: check email in login

--- src/client/components/Login/index.js
+++ src/client/components/Login/index.js
@@ -1,38 +1,53 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import izitoast from 'izitoast';
 
 import bind from 'Root/bind';
 import loginAct from 'Root/actions/login';
+import { e } from 'Root/libs/validator';
+
 import styles from './index.less';
 
 class Login extends Component {
   @bind
   login() {
-    this.props.dispatch(loginAct({
-      email: this.refs.email.value,
-      password: this.refs.password.value,
-      push: this.props.history.push
-    }));
+    if (e(this.refs.email.value)) {
+      this.props.dispatch(loginAct({
+        email: this.refs.email.value,
+        password: this.refs.password.value,
+        push: this.props.history.push
+      }));
+    } else {
+      izitoast.warning({
+        rtl: true,
+        title: 'ایمیل اشتباه است'
+      });
+      this.refs.email.focus();
+    }
   }
 
   render() {
     return (
-      <div className={styles.form}>
+      <div className={styles.container}>
 
-        <input
-          type='email'
-          ref='email'
-          placeholder='ایمیل'/>
+        <div className={styles.form}>
 
-        <input
-          type='password'
-          ref='password'
-          placeholder='رمز' />
+          <input
+            type='email'
+            ref='email'
+            placeholder='ایمیل'/>
 
-        <button onClick={this.login}>ورود</button>
+          <input
+            type='password'
+            ref='password'
+            placeholder='رمز' />
 
-        <a href='/signup'>ثبت نام کن</a>
-        <a href='/recovery'>رمزت رو فراموش کردی؟</a>
+          <button onClick={this.login}>ورود</button>
+
+          <a href='/signup'>ثبت نام کن</a>
+          <a href='/recovery'>رمزت رو فراموش کردی؟</a>
+
+        </div>
 
       </div>
     );
