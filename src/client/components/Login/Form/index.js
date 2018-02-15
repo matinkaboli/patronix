748afdc7937805d81003aed00d53e6fc84bda5bf
Matin Kaboli <matinkaboli@aol.com>
2018-02-15 23:08:22 +0330

    feat: move Login-Form to a new component in a new directory

--- src/client/components/Login/Form/index.js
+++ src/client/components/Login/Form/index.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import izitoast from 'izitoast';
+
+import bind from 'Root/bind';
+import loginAct from 'Root/actions/login';
+import { email } from 'Root/libs/validator';
 
 import styles from './index.less';
 
 class Form extends Component {
+  @bind
+  login() {
+    if (email(this.refs.email.value)) {
+      this.props.dispatch(loginAct({
+        email: this.refs.email.value,
+        password: this.refs.password.value,
+        push: this.props.history.push
+      }));
+    } else {
+      izitoast.warning({
+        rtl: true,
+        title: 'ایمیل اشتباه است'
+      });
+      this.refs.email.focus();
+    }
+  }
+
   render() {
     return (
       <div className={styles.form}>
@@ -28,4 +51,4 @@ class Form extends Component {
   }
 }
 
-export default Form;
+export default connect()(Form);
--- src/client/components/Login/index.js
+++ src/client/components/Login/index.js
@@ -1,33 +1,11 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import izitoast from 'izitoast';
 
-import bind from 'Root/bind';
-import loginAct from 'Root/actions/login';
 import Form from './Form';
 
-import { e } from 'Root/libs/validator';
 
 import styles from './index.less';
 
 class Login extends Component {
-  @bind
-  login() {
-    if (e(this.refs.email.value)) {
-      this.props.dispatch(loginAct({
-        email: this.refs.email.value,
-        password: this.refs.password.value,
-        push: this.props.history.push
-      }));
-    } else {
-      izitoast.warning({
-        rtl: true,
-        title: 'ایمیل اشتباه است'
-      });
-      this.refs.email.focus();
-    }
-  }
-
   render() {
     return (
       <div className={styles.container}>
@@ -37,4 +15,4 @@ class Login extends Component {
   }
 }
 
-export default connect()(Login);
+export default Login;
