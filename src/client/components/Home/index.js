3528e38692f6af79cc95520ce54e10ba8af7f4c2
Matin Kaboli <matinkaboli@aol.com>
2018-02-15 22:56:32 +0330

    fix: move Home/Menu to Components

--- src/client/components/Home/index.js
+++ src/client/components/Home/index.js
@@ -4,18 +4,26 @@ import { Link } from 'react-router-dom';
 
 import bind from 'Root/bind';
 import loginAct from 'Root/actions/login';
-import Menu from './Menu';
+import Menu from 'Components/Menu';
 import { email, password } from 'Libs/validator';
 
 class Home extends Component {
   @bind
   login() {
-    if (email(this.refs.email.value) && password(this.refs.password.value)) {
-      this.props.dispatch(loginAct({
-        email: this.refs.email.value,
-        password: this.refs.password.value,
-        push: this.props.history.push
-      }));
+    if (email(this.refs.email.value)) {
+      if (password(this.refs.password.value)) {
+        this.props.dispatch(loginAct({
+          email: this.refs.email.value,
+          password: this.refs.password.value,
+          push: this.props.history.push
+        }));
+      } else {
+        izitoast.warning({
+          rtl: true,
+          title: 'رمز باید حداقل هشت رقم باشد'
+        });
+        this.refs.password.focus();
+      }
     } else {
       izitoast.warning({
         rtl: true,
