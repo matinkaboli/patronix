6162c70ea37fad70c3ac685abb5c56475368cdff
Matin Kaboli <matinkaboli@aol.com>
2018-02-16 23:52:29 +0330

    feat: validate signup form

--- src/client/components/Signup/Form/index.js
+++ src/client/components/Signup/Form/index.js
@@ -3,19 +3,39 @@ import izitoast from 'izitoast';
 
 import bind from 'Root/bind';
 import loginAct from 'Root/actions/login';
-import { email } from 'Root/libs/validator';
+import { email, password } from 'Root/libs/validator';
 
 import styles from './index.less';
 
 class Form extends Component {
+  state = {
+    displayValidateError: false
+  };
+
   @bind
-  login() {
+  signup(e) {
+    e.preventDefault();
+
+    if (!e.target.checkValidity()) {
+      this.setState({ displayValidateError: true });
+      return;
+    }
+    this.setState({ displayValidateError: false });
+
     if (email(this.refs.email.value)) {
-      this.props.dispatch(loginAct({
-        email: this.refs.email.value,
-        password: this.refs.password.value,
-        push: this.props.history.push
-      }));
+      if (password(this.refs.password.value)) {
+        this.props.dispatch(loginAct({
+          email: this.refs.email.value,
+          password: this.refs.password.value,
+          push: this.props.history.push
+        }));
+      } else {
+        izitoast.warning({
+          rtl: true,
+          title: 'رمز باید حداقل هشت رقم باشد'
+        });
+        this.refs.password.focus();
+      }
     } else {
       izitoast.warning({
         rtl: true,
@@ -26,32 +46,41 @@ class Form extends Component {
   }
 
   render() {
+    const { displayValidateError } = this.state;
+
     return (
-      <div className={styles.form}>
+      <form className={`${styles.form}
+      ${displayValidateError ? 'displayValidateError' : ''}`}
+      onSubmit={this.signup}
+      noValidate>
 
         <input
           type='text'
           ref='fname'
+          required
           placeholder='نام' />
 
         <input
           type='text'
           ref='lname'
+          required
           placeholder='نام خانوادگی' />
 
         <input
           type='email'
           ref='email'
+          required
           placeholder='ایمیل' />
 
         <input
           type='password'
           ref='password'
+          required
           placeholder='رمز' />
 
-        <button onClick={this.signup}>ثبت نام</button>
+        <button>ثبت نام</button>
 
-      </div>
+      </form>
     );
   }
 }
