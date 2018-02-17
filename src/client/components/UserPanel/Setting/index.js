521fc251a1c97faa9e8187c4bf38327309bf3db8
Ali Movahedi <ali_movahedi@aol.com>
2018-02-17 11:37:54 +0330

    feat: show file error

--- src/client/components/UserPanel/Setting/index.js
+++ src/client/components/UserPanel/Setting/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import izitoast from 'izitoast';
 
 import bind from 'Root/bind';
 import setAvatar from 'Root/actions/setting/setAvatar';
@@ -12,6 +13,24 @@ class Setting extends Component {
     let { dispatch } = this.props;
 
     reader.addEventListener('loadend', () => {
+      if (file.size > 512000) {
+        izitoast.warning({
+          rtl: true,
+          title: 'حجم فایل باید حداکثر ۵۱۲ کیلوبایت باشد.'
+        });
+
+        return;
+      }
+
+      if (!['jpg', 'png'].includes(file.type)) {
+        izitoast.warning({
+          rtl: true,
+          title: 'فرمت فایل باید jpg یا png باشد'
+        });
+
+        return;
+      }
+
       dispatch(setAvatar({
         type: file.type.split('/')[1],
         size: file.size,
