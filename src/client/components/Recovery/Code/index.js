c696d2be7586a544caf1ef3442e888545401de56
Ali Movahedi <ali_movahedi@aol.com>
2018-02-19 22:11:25 +0330

    fix: fix recovery component

--- src/client/components/Recovery/Code/index.js
+++ src/client/components/Recovery/Code/index.js
@@ -1,16 +1,8 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
 
 class Code extends Component {
   render() {
-    // If the code is right
-    if (true) {
-      return <h1>Done</h1>;
-    }
-
-    localStorage.setItem('recovery_wrong_code', 1);
-
-    return <Redirect to='/' />;
+    return <h1>Done</h1>;
   }
 }
 
