bba3a02a5d14efc1c70e2f22af4fc08ff9dac437
Ali Movahedi <ali_movahedi@aol.com>
2018-02-20 23:07:57 +0330

    fix: logout from server-side

--- src/client/actions/logout.js
+++ src/client/actions/logout.js
@@ -1,9 +1,12 @@
 import { LOGOUT } from 'Root/actions';
+import socket from 'Root/socket';
 
 export default dispatch => {
   dispatch({
     type: LOGOUT
   });
+
+  socket.emit('logout');
 
   localStorage.token = '';
 };
