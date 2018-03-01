86f56eaabca78c758d20e0567a44f0e2c4ac6d08
Ali Movahedi <ali_movahedi@aol.com>
2018-03-02 00:34:34 +0330

    feat: add sites

--- src/client/components/Panel/Sites/index.js
+++ src/client/components/Panel/Sites/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import Box from 'Root/components/Box';
 import lazy from 'Root/js/lazy';
 import types from 'Root/actions';
+import Button from 'Root/components/Button';
 import styles from './index.less';
 
 class Sites extends Component {
@@ -11,12 +13,32 @@ class Sites extends Component {
     return (
       <div className={styles.container}>
         <Box>
-          <h3>
-            لیست سایت های شما:
-          </h3>
+          <div className={styles.header}>
+            <h3>
+              لیست سایت های شما:
+            </h3>
+            <Button>
+              افزودن سایت جدید
+            </Button>
+          </div>
+          {this.props.sites.map((v, i) =>
+            <div key={i}>
+              <p>{v.name}</p>
+              <Link to={`/panel/sites/${v.name}`}>
+                <Button>
+                  تنظیمات سایت
+                </Button>
+              </Link>
+            </div>
+          )}
         </Box>
-        <Box>
 
+        <Box>
+          <div className={styles.header}>
+            <h3>
+              لیست سایت هایی که شما در آن اوپراتور هستید:
+            </h3>
+          </div>
         </Box>
       </div>
     );
