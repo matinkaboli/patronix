64e22f5b7eb0e1ed573e424d3e44188063a328f8
Ali Movahedi <ali_movahedi@aol.com>
2018-03-02 15:44:22 +0330

    feat: add setting/avatar/remove event

--- src/sockets/client/setting/avatar/remove.js
+++ src/sockets/client/setting/avatar/remove.js
@@ -5,13 +5,16 @@ import middlewares from 'Root/middlewares';
 let socket = new SocketEvent();
 
 socket
-.namespace('operator')
+.namespace('client')
 .name('setting/avatar/remove')
 .middleware(
   middlewares.client.checkToken
 )
-.handler(socket => () => {
+.handler(socket => async () => {
+  socket.data.user.avatar = null;
+  await socket.data.user.save();
 
+  socket.emit('setting/avatar/remove', 200);
 });
 
 export default socket;
