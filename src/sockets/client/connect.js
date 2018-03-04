bac877f4dc48ebede9fac5b7b39d861c7127a64e
Ali Movahedi <ali_movahedi@aol.com>
2018-03-04 22:23:11 +0330

    feat: add user to socketstore after connecting

--- src/sockets/client/connect.js
+++ src/sockets/client/connect.js
@@ -1,12 +1,28 @@
 import { SocketEvent } from 'socket.io-manager';
 
+import { SocketStore } from 'Root/models';
+import middlewares from 'Root/middlewares';
+
 let socket = new SocketEvent();
 
 socket
 .namespace('client')
 .name('connect')
-.handler(socket => () => {
-  console.log(socket.id);
+.middleware(
+  middlewares.client.checkToken
+)
+.handler(socket => async () => {
+  let store = SocketStore.findOne({ user: socket.data.user._id });
+  if (store) {
+    await store.remove();
+  }
+
+  store = new SocketStore({
+    socket: socket.id,
+    user: socket.data.user._id
+  });
+
+  await store.save();
 });
 
 export default socket;
