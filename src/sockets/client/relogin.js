de7237a627fb83ab4d36f448359c3f790a67e501
Ali Movahedi <ali_movahedi@aol.com>
2018-03-06 20:48:29 +0330

    feat: send invitation on login

--- src/sockets/client/relogin.js
+++ src/sockets/client/relogin.js
@@ -1,5 +1,6 @@
 import { SocketEvent } from 'socket.io-manager';
 
+import { Invitation } from 'Root/models';
 import middlewares from 'Root/middlewares';
 
 let socket = new SocketEvent();
@@ -10,14 +11,23 @@ socket
 .middleware(
   middlewares.client.checkToken
 )
-.handler(socket => () => {
+.handler(socket => async () => {
+  let invitations = await Invitation
+  .find({ user: socket.data.user._id })
+  .populate({ path: 'from', select: 'name' })
+  .exec();
+
   socket.emit('relogin', 200,
   {
     user: {
       name: socket.data.user.name,
       email: socket.data.user.email,
       avatar: socket.data.user.avatar.url
-    }
+    },
+    invitations: invitations.map(i => ({
+      from: i.from.name,
+      code: i.code
+    }))
   });
 });
 
