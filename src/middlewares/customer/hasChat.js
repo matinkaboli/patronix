a8cb8ebb0ecce655a08184585f8293d9eef48e31
Ali Movahedi <ali_movahedi@aol.com>
2018-03-08 11:01:22 +0330

    feat: send new chat notification to operators

--- src/middlewares/customer/hasChat.js
+++ src/middlewares/customer/hasChat.js
@@ -1,20 +1,30 @@
-import { Chat } from 'Root/models';
+import { Chat, ClientToken } from 'Root/models';
 
-export default (next, socket) => async message => {
+export default (next, socket, nsp, io) => async message => {
   if (!socket.data.chat) {
     let chat = new Chat({
       site: socket.data.site._id
     });
     await chat.save();
-
     socket.data.chat = chat;
+
+    let operators = await ClientToken.find({
+      user: {
+        $in: socket.data.site.operators
+      }
+    }, { token: 1 });
+
+    for (let { token } of operators) {
+      io.of('/client').to(token).emit('newchat', message);
+    }
+
     next();
   }
 
   else {
     let chat = await Chat.findById(socket.data.chat._id);
     socket.data.chat = chat;
-    
+
     next();
   }
 };
