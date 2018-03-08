6a3b9cd945d7102bfaf14b43bb5cb06d1ac8dd6d
Ali Movahedi <ali_movahedi@aol.com>
2018-03-08 10:35:40 +0330

    feat: add chats to Chat model

--- src/models/Chat.js
+++ src/models/Chat.js
@@ -1,11 +1,34 @@
 import mongoose, { Schema } from 'mongoose';
 
+const chats = new Schema({
+  sender: {
+    enum: [0, 1], // 0: customer, 1: operator
+    type: Number,
+    required: [true, 'Sender required']
+  },
+  message: {
+    type: String,
+    trim: true,
+    required: [true, 'Message required'],
+    minlength: 1,
+    maxlength: 250
+  },
+  time: {
+    type: Date,
+    trim: true,
+    default: Date.now
+  }
+}, {
+  _id: false
+});
+
 const schema = new Schema({
   operator: {
     type: Schema.Types.ObjectId,
     ref: 'User',
     unique: true
-  }
+  },
+  chats: [chats]
 });
 
 export default mongoose.model('Chat', schema);
