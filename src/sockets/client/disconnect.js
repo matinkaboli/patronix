8a7c9aafeb824746e8e7027de9bdd49667883c49
Ali Movahedi <ali_movahedi@aol.com>
2018-03-11 18:28:30 +0330

    feat: remove socket from store after disconnecting

--- src/sockets/client/disconnect.js
+++ src/sockets/client/disconnect.js
@@ -1,6 +1,7 @@
 import { SocketEvent } from 'socket.io-manager';
 
 import middlewares from 'Root/middlewares';
+import { SocketStore } from 'Root/models';
 
 let socket = new SocketEvent();
 
@@ -11,6 +12,19 @@ socket
   middlewares.client.checkToken
 )
 .handler(socket => async () => {
+  let ss = await SocketStore.findOne({ user: socket.data.user._id });
+
+  if (ss) {
+    let index = ss.sockets.findIndex(i => i === socket.id);
+    ss.sockets.splice(index, index + 1);
+
+    if (!ss.sockets.length) {
+      await ss.remove();
+      return;
+    }
+    
+    await ss.save();
+  }
 });
 
 export default socket;
