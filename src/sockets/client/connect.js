2b861c322bb14874f2a859e4fee910fba424c60b
Ali Movahedi <ali_movahedi@aol.com>
2018-03-11 13:42:43 +0330

    feat: add socket to socket store on connection

--- src/sockets/client/connect.js
+++ src/sockets/client/connect.js
@@ -1,6 +1,7 @@
 import { SocketEvent } from 'socket.io-manager';
 
 import middlewares from 'Root/middlewares';
+import { SocketStore } from 'Root/models';
 
 let socket = new SocketEvent();
 
@@ -12,6 +13,21 @@ socket
 )
 .handler(socket => async () => {
   socket.join(socket.token.token);
+
+  let ss = await SocketStore.findOne({ user: socket.data.user._id });
+
+  if (ss) {
+    ss.sockets.push(socket.id);
+    await ss.save();
+  }
+
+  else {
+    ss = new SocketStore({
+      user: socket.data.user._id,
+      sockets: [socket.id]
+    });
+    await ss.save();
+  }
 });
 
 export default socket;
