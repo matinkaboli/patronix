29129ec6a3a37ff31f9eadf52d231fa5f11caa7e
Ali Movahedi <ali_movahedi@aol.com>
2018-03-12 20:48:10 +0330

    fix: fix send notification for new chats

--- src/middlewares/customer/hasChat.js
+++ src/middlewares/customer/hasChat.js
@@ -1,4 +1,4 @@
-import { Chat, ClientToken } from 'Root/models';
+import { Chat } from 'Root/models';
 
 export default (next, socket, nsp, io) => async message => {
   if (!socket.data.chat) {
@@ -8,18 +8,10 @@ export default (next, socket, nsp, io) => async message => {
     await chat.save();
     socket.data.chat = chat;
 
-    let operators = await ClientToken.find({
-      user: {
-        $in: socket.data.site.operators
-      }
-    }, { token: 1 });
-
-    for (let { token } of operators) {
-      io
-      .of('/client')
-      .to(token)
-      .emit('newchat', message);
-    }
+    io
+    .of('/client')
+    .to(socket.data.site._id.toString())
+    .emit('newchat', message);
 
     socket.join(chat._id.toString());
 
