9cf7e505f644be11631110b53e6de19c19b00d9c
Matin Kaboli <matinkaboli@aol.com>
2018-03-13 22:39:32 +0330

    redirect after activate

--- src/client/components/Public/Activate/index.js
+++ src/client/components/Public/Activate/index.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
+import izitoast from 'izitoast';
 
 import lazy from 'Root/js/lazy';
 
 class Activate extends Component {
   render() {
+    izitoast.success({
+      rtl: true,
+      title: 'حساب شما با موفقیت تایید شد'
+    });
+
     return <Redirect to='/login' />;
   }
 }
