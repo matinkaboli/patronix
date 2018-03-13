f41a3223c31fea0208560626408037badbd1cba2
Ali Movahedi <ali_movahedi@aol.com>
2018-03-13 21:37:08 +0330

    fix: fix Panel UI bug

--- src/client/components/Panel/index.js
+++ src/client/components/Panel/index.js
@@ -13,14 +13,16 @@ class Panel extends Component {
       <div className={styles.container}>
         <Sidebar />
 
-        <Switch>
-          <Route exact path='/panel' component={HomePanel} />
-          <Route path='/panel/setting' component={Setting} />
-          <Route path='/panel/sites' component={Sites} />
-          <Route>
-            <Redirect to='/notfound' />
-          </Route>
-        </Switch>
+        <div className={styles.content}>
+          <Switch>
+            <Route exact path='/panel' component={HomePanel} />
+            <Route path='/panel/setting' component={Setting} />
+            <Route path='/panel/sites' component={Sites} />
+            <Route>
+              <Redirect to='/notfound' />
+            </Route>
+          </Switch>
+        </div>
       </div>
     );
   }
