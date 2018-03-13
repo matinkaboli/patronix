2a62967586f90fecd1c8715f3128e0211791fbe5
Ali Movahedi <ali_movahedi@aol.com>
2018-03-13 09:23:45 +0330

    style: change hasValidChat style

--- src/middlewares/client/hasValidChat.js
+++ src/middlewares/client/hasValidChat.js
@@ -1,11 +1,13 @@
 import { Chat } from 'Root/models';
 
 export default (next, socket) => async () => {
-  if (socket.data.chat) {
-    socket.data.chat = await Chat.findById(socket.data.chat._id);
+  if (!socket.data.chat) {
+    return;
+  }
+  
+  socket.data.chat = await Chat.findById(socket.data.chat._id);
 
-    if (!socket.data.chat.done) {
-      next();
-    }
+  if (!socket.data.chat.done) {
+    next();
   }
 };
