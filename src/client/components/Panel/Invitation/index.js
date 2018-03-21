675a888e81903d761e75373e3262719a57064f85
Matin Kaboli <matinkaboli@aol.com>
2018-03-21 22:12:00 +0330

    feat: say you have no new invitations

--- src/client/components/Panel/Invitation/index.js
+++ src/client/components/Panel/Invitation/index.js
@@ -3,18 +3,26 @@ import { connect } from 'react-redux';
 
 import Box from 'Root/components/Box';
 
+import styles from './index.less';
+
 
 class Invitation extends Component {
   render() {
+    console.log(this.props.invitations);
     return (
       <Box>
-        {this.props.invitations.map((v, i) => {
+        <h1 className={styles.title}>
+          {this.props.invitations.length ?
+          'دعوت ها' :
+          'شما در هیچ سایتی دعوت نشده اید'}
+        </h1>
+        {this.props.invitations.map((v, i) =>
           <div key={i}>
             <p>From: {v.from}</p>
             <p>Code: {v.code}</p>
             <hr />
-          </div>;
-        })}
+          </div>
+        )}
       </Box>
     );
   }
