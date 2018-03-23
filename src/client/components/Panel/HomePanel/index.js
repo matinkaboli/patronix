12949309f3593530d59d71d84ef93c203bf16a4c
Matin Kaboli <matinkaboli@aol.com>
2018-03-23 16:26:33 +0430

    feat: add some shit to /panel

--- src/client/components/Panel/HomePanel/index.js
+++ src/client/components/Panel/HomePanel/index.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import Box from 'Root/components/Box';
+
+import styles from './index.less';
 
 
 class Index extends Component {
   render() {
-    return <p>home</p>;
+    return (
+      <div>
+        <div className={styles.container}>
+          <Box>
+            <p>اطلاعاتت</p>
+          </Box>
+          <Box>
+            <p>سایت هات</p>
+          </Box>
+          <Box>
+            <p>چت هات</p>
+          </Box>
+        </div>
+      </div>
+    );
   }
 }
 
-export default Index;
+export default connect(
+  state => ({
+    user: state.user
+  })
+)(Index);
