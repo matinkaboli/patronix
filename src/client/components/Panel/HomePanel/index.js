14631e7b4f0f3bb097bf27731333c1e3317cfa16
Matin Kaboli <matinkaboli@aol.com>
2018-03-24 00:14:34 +0430

    feat: add Roboto font

--- src/client/components/Panel/HomePanel/index.js
+++ src/client/components/Panel/HomePanel/index.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import bind from 'Root/js/bind';
+
 import Box from 'Root/components/Box';
 
+import defaultImage from 'Root/images/user-default.png';
 import styles from './index.less';
 
 
 class Index extends Component {
+  @bind
+  renderImage() {
+    if (this.props.user.avatar) {
+      return (
+        <img
+        src={this.props.user.avatar}
+        alt='عکس کاربر'
+        className={styles.avatarImage} />
+      );
+    }
+
+    return (
+      <img
+        src={defaultImage}
+        alt='عکس کاربر'
+        className={styles.avatarImage} />
+    );
+  }
+
   render() {
     return (
       <div>
         <div className={styles.container}>
           <Box>
-            <p>اطلاعاتت</p>
+            {this.renderImage()}
+            <h2 className={styles.name}>{this.props.user.name}</h2>
           </Box>
           <Box>
             <p>سایت هات</p>
