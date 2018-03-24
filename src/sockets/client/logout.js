f13f07caea88e85e44884cb743d8daf2c82e89e1
Ali Movahedi <ali_movahedi@aol.com>
2018-03-24 12:49:08 +0430

    feat: send event when a user logout from site

--- src/sockets/client/logout.js
+++ src/sockets/client/logout.js
@@ -1,7 +1,7 @@
 import { SocketEvent } from 'socket.io-manager';
 
 import middlewares from 'Root/middlewares';
-import { ClientToken } from 'Root/models';
+import { ClientToken, SocketStore, Site } from 'Root/models';
 
 let socket = new SocketEvent();
 
@@ -11,10 +11,24 @@ socket
 .middleware(
   middlewares.client.checkToken
 )
-.handler(socket => async () => {
+.handler((socket, nsp, io) => async () => {
   await ClientToken.remove({ token: socket.handshake.query.token });
 
   socket.handshake.query.token = '';
+
+  let ss = await SocketStore.findOne({ user: socket.data.user._id });
+  await ss.remove();
+
+  let sites = await Site.find(
+    { operators: socket.data.user._id },
+    { _id: 1 }
+  );
+  for (let site of sites) {
+    io
+    .of('/customer')
+    .to(site._id.toString())
+    .emit('goesOffline');
+  }
 
   socket.emit('logout', 200);
 });
