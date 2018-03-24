83fc888c4c169cac917c064d71ddf4972e146839
Ali Movahedi <ali_movahedi@aol.com>
2018-03-24 14:51:44 +0430

    feat: add error handler for customer message
    feat: clear input after sending message

--- src/customer/components/Content/Footer/index.js
+++ src/customer/components/Content/Footer/index.js
@@ -2,12 +2,41 @@ import React, { Component } from 'react';
 
 import bind from 'Root/js/bind';
 import send from 'Root/actions/send';
+import { dispatch } from 'Root/store';
+import types from 'Root/actions';
 import styles from './index.less';
 
 class Footer extends Component {
   @bind
   send() {
-    send(this.refs.message.value);
+    let message = this.refs.message.value;
+
+    if (message.length > 250) {
+      dispatch({
+        type: types.chats.ADD,
+        chat: {
+          type: 'warning',
+          sender: 'customer',
+          text: 'بیشتر از ۲۵۰ کاراکتر نمی توانید ارسال کنید.'
+        }
+      });
+      return;
+    }
+
+    if (message.length === 0) {
+      dispatch({
+        type: types.chats.ADD,
+        chat: {
+          type: 'warning',
+          sender: 'customer',
+          text: 'پیام خالی نمی توانید ارسال کنید.'
+        }
+      });
+      return;
+    }
+
+    send(message);
+    this.refs.message.value = '';
   }
 
   @bind
