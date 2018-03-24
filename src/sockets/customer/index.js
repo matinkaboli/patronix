ffd262ecb22487beed9aa8d9c8fb4990252bcba8
Ali Movahedi <ali_movahedi@aol.com>
2018-03-24 11:31:41 +0430

    feat: combine getOnlines and init together

--- src/sockets/customer/index.js
+++ src/sockets/customer/index.js
@@ -1,7 +1,6 @@
 export default [
   require('./init'),
   require('./message'),
-  require('./getOnlines'),
   require('./disconnect'),
   require('./finish')
 ];
--- src/sockets/customer/init.js
+++ src/sockets/customer/init.js
@@ -1,6 +1,7 @@
 import { SocketEvent } from 'socket.io-manager';
 
 import middlewares from 'Root/middlewares';
+import { SocketStore } from 'Root/models';
 
 let socket = new SocketEvent();
 
@@ -10,14 +11,27 @@ socket
 .middleware(
   middlewares.customer.checkToken
 )
-.handler(socket => () => {
+.handler(socket => async () => {
   if (!socket.inited) {
     socket.join(socket.data.site._id.toString());
 
+    let state = {
+      online: [],
+      offline: []
+    };
+
+    for (let operator of socket.data.site.operators) {
+      let ss = await SocketStore.findOne({ user: operator });
+      state[ss ? 'online' : 'offline'].push(ss.user);
+    }
+
+    socket.emit('init', 200, state);
+
     socket.inited = true;
+    socket.data.state = state;
   }
-  
-  socket.emit('init', 200);
+
+  socket.emit('init', 200, socket.data.state);
 });
 
 export default socket;
