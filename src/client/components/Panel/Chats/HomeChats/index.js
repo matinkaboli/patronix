73456af552029ffe53856698a0f51b7fcb6f32e4
Matin Kaboli <matinkaboli@aol.com>
2018-03-26 00:46:09 +0430

    feat: /panel/chats

--- src/client/components/Panel/Chats/HomeChats/index.js
+++ src/client/components/Panel/Chats/HomeChats/index.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import Box from 'Root/components/Box';
+
+import styles from './index.less';
 
 
 class HomeChat extends Component {
   render() {
-    return <h1>Home Chats</h1>;
+    return (
+      <Box className={styles.container}>
+        <div className={styles.chat}>
+          <Link to='/panel/chats/'>سایتییت</Link>
+        </div>
+      </Box>
+    );
   }
 }
 
