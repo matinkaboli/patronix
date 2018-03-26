01e0026753c631244e349e06dc73b82ff1e7a478
Ali Movahedi <ali_movahedi@aol.com>
2018-03-26 16:13:24 +0430

    perf: send just _id

--- src/routerHandler/chats/history.js
+++ src/routerHandler/chats/history.js
@@ -6,6 +6,6 @@ export async function handler(socket) {
   let site = await Site.find({ owner: socket.data.user._id }, { _id: 1 })._id;
   return {
     status: 200,
-    data: await Chat.find({ site, done: true })
+    data: await Chat.find({ site, done: true }, { _id: 1 })
   };
 }
