383ab57ba9c4af22db3bb67976b6e00af3928532
Ali Movahedi <ali_movahedi@aol.com>
2018-03-31 11:57:46 +0430

    feat: send more specified data to chats/history

--- src/routerHandler/chats/history.js
+++ src/routerHandler/chats/history.js
@@ -4,8 +4,24 @@ export const path = '/panel/chats/history';
 
 export async function handler(socket) {
   let site = await Site.findOne({ owner: socket.data.user._id }, { _id: 1 });
+  let chats = await Chat
+  .find({ site: site._id, done: true }, { _id: 1, chats: 1, operator: 1 })
+  .populate({ path: 'operator.id', select: ['email', 'name'] })
+  .lean()
+  .exec();
+
+  chats = chats.map(i => (
+    {
+      ...i,
+      operator: {
+        name: i.operator.id.name,
+        email: i.operator.id.email
+      }
+    }
+  ));
+
   return {
     status: 200,
-    data: await Chat.find({ site: site._id, done: true }, { _id: 1, chats: 1 })
+    data: chats
   };
 }
