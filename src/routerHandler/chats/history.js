d6b76dcfe7bca95542f80418b93ea140a6c68a5b
Ali Movahedi <ali_movahedi@aol.com>
2018-03-31 11:19:47 +0430

    feat: send chats

--- src/routerHandler/chats/history.js
+++ src/routerHandler/chats/history.js
@@ -3,9 +3,9 @@ import { Chat, Site } from 'Root/models';
 export const path = '/panel/chats/history';
 
 export async function handler(socket) {
-  let site = await Site.find({ owner: socket.data.user._id }, { _id: 1 })._id;
+  let site = await Site.findOne({ owner: socket.data.user._id }, { _id: 1 });
   return {
     status: 200,
-    data: await Chat.find({ site, done: true }, { _id: 1 })
+    data: await Chat.find({ site: site._id, done: true }, { _id: 1, chats: 1 })
   };
 }
