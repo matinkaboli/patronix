38e041ad1dfab4c6c85cedc3a9321994fa86f1b4
Matin Kaboli <matinkaboli@aol.com>
2018-04-01 19:43:43 +0430

    lint

--- src/client/components/Panel/Chats/History/index.js
+++ src/client/components/Panel/Chats/History/index.js
@@ -11,30 +11,29 @@ import styles from './index.less';
 
 class History extends Component {
   render() {
-    const chats = this.props.historyChats;
-    console.log(chats);
-
     return (
       <div className={styles.container}>
-        {chats.length === 0 && <h1 className={styles.title}>
+        {this.props.historyChats.length === 0 && <h1 className={styles.title}>
           چت های پیشینی وجود ندارد
         </h1>}
 
-        {chats.map((v, i) => <Link to={`/panel/chats/${v._id}`}>
-          <div className={styles.chat} key={i}>
-          <h1>سایت: {v.site.name}</h1>
+        {this.props.historyChats.map((v, i) =>
+          <Link to={`/panel/chats/${v._id}`}>
+            <div className={styles.chat} key={i}>
+            <h1>سایت: {v.site.name}</h1>
 
-          <span>
-            {new Date(v.chats[0].time).getHours()}:
-            {new Date(v.chats[0].time).getMinutes()}
-          </span>
+            <span>
+              {new Date(v.chats[0].time).getHours()}:
+              {new Date(v.chats[0].time).getMinutes()}
+            </span>
 
-          <p>پیام: {v.chats[0].message}</p>
+            <p>پیام: {v.chats[0].message}</p>
 
-          <p>توسط پشتیبان: {v.operator.name}</p>
+            <p>توسط پشتیبان: {v.operator.name}</p>
 
-          </div>
-        </Link>)}
+            </div>
+          </Link>
+        )}
       </div>
     );
   }
