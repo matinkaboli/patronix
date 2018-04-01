4ba97a01050401972b58239b03f817f992a2c2eb
Matin Kaboli <matinkaboli@aol.com>
2018-04-01 19:42:56 +0430

    fix: historyChats style and data

--- src/client/components/Panel/Chats/History/index.js
+++ src/client/components/Panel/Chats/History/index.js
@@ -6,33 +6,36 @@ import types from 'Root/actions';
 
 import lazy from 'Root/js/lazy';
 
-import Box from 'Root/components/Box';
-
 import styles from './index.less';
 
 
 class History extends Component {
   render() {
     const chats = this.props.historyChats;
+    console.log(chats);
 
     return (
-      <Box className={styles.container}>
+      <div className={styles.container}>
         {chats.length === 0 && <h1 className={styles.title}>
           چت های پیشینی وجود ندارد
         </h1>}
 
-        {chats.map((v, i) => <div className={styles.chat} key={i}>
-          <Link to={`/panel/chats/${v._id}`}>{v.message.text}</Link>
-
-          <span>از سایت: {v.from}</span>
+        {chats.map((v, i) => <Link to={`/panel/chats/${v._id}`}>
+          <div className={styles.chat} key={i}>
+          <h1>سایت: {v.site.name}</h1>
 
           <span>
-            {new Date(v.message.time).getHours()}:
-            {new Date(v.message.time).getMinutes()}
+            {new Date(v.chats[0].time).getHours()}:
+            {new Date(v.chats[0].time).getMinutes()}
           </span>
 
-        </div>)}
-      </Box>
+          <p>پیام: {v.chats[0].message}</p>
+
+          <p>توسط پشتیبان: {v.operator.name}</p>
+
+          </div>
+        </Link>)}
+      </div>
     );
   }
 }
