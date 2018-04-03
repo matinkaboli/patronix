8126ca7527f7a0b41d2f6fa4af09bdde8e4a3050
Ali Movahedi <ali_movahedi@aol.com>
2018-04-03 14:20:36 +0430

    feat: send mail to fucking user

--- src/sockets/client/signup.js
+++ src/sockets/client/signup.js
@@ -2,8 +2,9 @@ import { SocketEvent } from 'socket.io-manager';
 import uid from 'uuid/v4';
 
 import { User, AL } from 'Root/models';
-import { dbkey } from 'Root/config';
+import { dbkey, url } from 'Root/config';
 import { hmac } from 'Root/crypt';
+import sendMail from 'Root/sendMail';
 
 let socket = new SocketEvent();
 
@@ -11,12 +12,19 @@ socket
 .namespace('/client')
 .name('signup')
 .handler(socket => async (data, captcha) => {
+  data.email = data.email.toLowerCase();
+
   if (captcha.toLowerCase() !== socket.data.captcha) {
     socket.emit('signup', 400, 0);
     return;
   }
 
-  let user = new User({
+  let user = await User.findOne({ email: data.email });
+  if (user) {
+    socket.emit('signup', 400, 2);
+  }
+
+  user = new User({
     name: data.name,
     email: data.email,
     password: hmac(data.password, dbkey),
@@ -33,6 +41,16 @@ socket
     });
 
     await al.save();
+
+    await sendMail({
+      to: user.email,
+      subject: 'لینک فعال سازی',
+      html: `
+      برای فعال ساری حساب کاربری خود لینک زیر را لمس کنید
+      <a href='${url}/activate/${al.code}'>لینک</a>
+      `
+    });
+
     socket.emit('signup', 200);
   } catch (e) {
     socket.emit('signup', 400, 1);
