4400eff065d7a0b4ebd45e1b7ebfb336a1d097be
Ali Movahedi <ali_movahedi@aol.com>
2018-04-09 21:50:51 +0430

    fix: fix production setup

--- src/config.js
+++ src/config.js
@@ -4,8 +4,8 @@ export default {
   uploadDir: resolve(__dirname, 'static/uploads'),
   otkey: 'l4E7MDeLCt3SbrIoY2UHWBiONVh9f1RPGvg',
   dbkey: 'Me1vfSudFwiPqhl78yRbTA1kW9VoIZ',
-  db: 'mongodb://localhost/patronix-dev',
-  url: 'http://localhost:8010',
+  db: 'mongodb://localhost/patronix',
+  url: 'http://patronix.ir',
   title: 'Patronix',
-  port: '8010'
+  port: '80'
 };
