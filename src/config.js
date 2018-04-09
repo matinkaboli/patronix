7e8781820f5b683bdf9b5e71e2faca42d43297a6
Ali Movahedi <ali_movahedi@aol.com>
2018-04-10 00:18:43 +0430

    make db dynamic

--- src/config.js
+++ src/config.js
@@ -4,16 +4,16 @@ let config = {
   uploadDir: resolve(__dirname, 'static/uploads'),
   otkey: 'l4E7MDeLCt3SbrIoY2UHWBiONVh9f1RPGvg',
   dbkey: 'Me1vfSudFwiPqhl78yRbTA1kW9VoIZ',
-  db: 'mongodb://localhost/patronix-dev',
+  db: 'mongodb://localhost/patronix',
   prodUrl: 'https://patronix.ir',
   devUrl: 'http://localhost:8010',
   title: 'Patronix',
-  port: '443',
-  devPort: '8010',
+  port: '443'
 };
 
 if (process.env.NODE_ENV === 'development') {
-  config.port = config.devPort;
+  config.port = '8010';
+  config.db = 'mongodb://localhost/patronix-dev';
 }
 
 export default config;
