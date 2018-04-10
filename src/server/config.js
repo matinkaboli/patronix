379fc947989540ff6d2af154b3ee85ba6aaff2dc
Ali Movahedi <ali_movahedi@aol.com>
2018-04-10 15:54:29 +0430

    fix: sending correct url to client

--- src/server/config.js
+++ src/server/config.js
@@ -5,7 +5,7 @@ let config = {
   otkey: 'l4E7MDeLCt3SbrIoY2UHWBiONVh9f1RPGvg',
   dbkey: 'Me1vfSudFwiPqhl78yRbTA1kW9VoIZ',
   db: 'mongodb://localhost/patronix',
-  prodUrl: 'https://patronix.ir',
+  url: 'https://patronix.ir',
   devUrl: 'http://localhost:8010',
   title: 'Patronix',
   port: '443',
