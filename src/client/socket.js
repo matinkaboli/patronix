4791da11b27ad5abff026dc475a895a13b8574b2
Ali Movahedi <ali_movahedi@aol.com>
2018-05-14 21:13:20 +0430

    feat: add cocket for development

--- src/client/socket.js
+++ src/client/socket.js
@@ -4,6 +4,12 @@ let socket = io('/client', { query: `token=${localStorage.token}` });
 
 if (process.env.NODE_ENV === 'development') {
   window.socket = socket;
+  window.cocket = (ev, ...args) => {
+    socket.emit(ev, ...args);
+    socket.once(ev, (...args) => {
+      console.log(...args);
+    });
+  };
 }
 
 export default socket;
