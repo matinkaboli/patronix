0bebd6ebfb2764c2f1243c5d5e0c3b292e313ea6
Matin Kaboli <matinkaboli@aol.com>
2018-05-17 19:34:55 +0430

    refactor: put Operators list and invite fields to a new directory

---
+++ src/client/components/Panel/Sites/Setting/Operators/Invite/index.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import izitoast from 'izitoast';
+
+import inviteOperator from 'Root/actions/user/site/operator/invite';
+
+import { email } from 'Root/js/validator';
+import bind from 'Root/js/bind';
+
+import Field from 'Root/components/Panel/Field';
+import Button from 'Root/components/Button';
+
+
+class Invite extends Component {
+  @bind
+  newOperator() {
+    if (!this.refs.newOperator.value) {
+      izitoast.warning({
+        rtl: true,
+        title: 'مقادیر کافی نیستند'
+      });
+      return;
+    }
+
+    if (!email(this.refs.newOperator.value)) {
+      izitoast.error({
+        rtl: true,
+        title: 'ایمیل صحیح نمیباشد'
+      });
+      return;
+    }
+
+    inviteOperator(this.refs.newOperator.value);
+
+    this.refs.newOperator.value = '';
+  }
+
+  render() {
+    return (
+      <Field>
+        <div>
+          <input
+            type='email'
+            ref='newOperator'
+            placeholder='ایمیل پشتیبان'
+          />
+        </div>
+
+        <div>
+          <Button
+            color='blue'
+            handleClick={this.newOperator}>
+            اضافه کردن
+          </Button>
+        </div>
+      </Field>
+    );
+  }
+}
+
+
+export default Invite;
---
+++ src/client/components/Panel/Sites/Setting/Operators/List/index.js
@@ -0,0 +1,58 @@
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+
+import removeOperator from 'Root/actions/user/site/operator/remove';
+
+import assure from 'Root/js/assure';
+import bind from 'Root/js/bind';
+
+import Field from 'Root/components/Panel/Field';
+import Button from 'Root/components/Button';
+
+
+class List extends Component {
+  @bind
+  removeOperator(email) {
+    return () => {
+      assure(() => {
+        this.props.dispatch(removeOperator(email));
+      });
+    };
+  }
+
+  render() {
+    return (
+      <Fragment>
+        {this.props.operators ?
+          this.props.operators.map((v, i) =>
+          <Field key={i}>
+            <div>
+              <p>نام: {v.name}</p>
+              <p>ایمیل: {v.email}</p>
+            </div>
+
+            {this.props.owner === v._id ? '' :
+              <div>
+                <Button
+                  color='red'
+                  handleClick={this.removeOperator(v.email)}>
+                    حذف پشتیبان
+                </Button>
+              </div>
+            }
+
+          </Field>
+        ) : null
+        }
+      </Fragment>
+    );
+  }
+}
+
+
+export default connect(
+  state => ({
+    operators: state.site.operators,
+    owner: state.site.owner
+  })
+)(List);
