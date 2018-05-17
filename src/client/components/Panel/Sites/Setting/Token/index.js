f5adc3f910a9fd5dacf4db9cddca0ceec1b0aef2
Matin Kaboli <matinkaboli@aol.com>
2018-05-17 19:35:23 +0430

    refactor: put Token field to a new directory

---
+++ src/client/components/Panel/Sites/Setting/Token/index.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import revokeToken from 'Root/actions/user/site/revokeToken';
+
+import bind from 'Root/js/bind';
+
+import Field from 'Root/components/Panel/Field';
+import Button from 'Root/components/Button';
+
+import styles from './index.less';
+
+
+class Token extends Component {
+  @bind
+  revokeToken() {
+    this.props.dispatch(revokeToken());
+  }
+
+  render() {
+    return (
+      <Field>
+        <div>
+          <p>توکن</p>
+          <p>{this.props.token}</p>
+          <code className={styles.script}>
+            {
+            `<script
+            id='patronix-data'
+            token=${this.props.token}
+            src="${localStorage.getItem('patronixUrl')}/static/js/customer.js"
+            ></script>`
+            }
+          </code>
+        </div>
+
+        <div>
+          <Button
+            color='blue'
+            handleClick={this.revokeToken}>
+            توکن جدید
+          </Button>
+        </div>
+      </Field>
+    );
+  }
+}
+
+
+export default connect(
+  state => ({
+    token: state.site.token
+  })
+)(Token);
